18a055f092e8e05e9acd9a00de8899f0762adcc2
удвщкфвщ <marson4ik2019@gmail.com>
2020-08-19 02:47:04 +0300

    Updates

---
+++ precache-manifest.783bb04689ca7edc96bdd9d36fae601e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe777e9e645cacb4056882549fe5154",
+    "url": "/TestProject-launchesPast/index.html"
+  },
+  {
+    "revision": "3bedf6d7de885b26f022",
+    "url": "/TestProject-launchesPast/static/css/main.14a82d68.chunk.css"
+  },
+  {
+    "revision": "e1db39258c4c6f854ef9",
+    "url": "/TestProject-launchesPast/static/js/2.c60cefc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TestProject-launchesPast/static/js/2.c60cefc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bedf6d7de885b26f022",
+    "url": "/TestProject-launchesPast/static/js/main.983a7090.chunk.js"
+  },
+  {
+    "revision": "164f598eaa598500aa81",
+    "url": "/TestProject-launchesPast/static/js/runtime-main.54f7ddfd.js"
+  }
+]);
